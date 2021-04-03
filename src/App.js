1eef9f328c0be319ade05fab4b9d7aca6040e949
Anmolkumar Shah <anmolshah.20k@gmail.com>
2021-04-03 12:05:08 +0530

    Anmol - initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="display-1 mx-auto">DatAlgo - </div>{" "}
+        <span className="display-4">
+          A Visualizing tool to Visualize Data Structure & Algorithm
+        </span>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
