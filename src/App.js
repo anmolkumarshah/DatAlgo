bb371db40e109504bafff552081fc950d2a9f0c1
Anmolkumar Shah <anmolshah.20k@gmail.com>
2021-04-03 12:28:14 +0530

    Anmol - readme updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <React.Fragment>
         <div className="display-1 mx-auto">DatAlgo - </div>{" "}
         <span className="display-4">
-          A Visualizing tool to Visualize Data Structure & Algorithm
+          A tool to Visualize Data Structure & Algorithm
         </span>
       </React.Fragment>
     );
