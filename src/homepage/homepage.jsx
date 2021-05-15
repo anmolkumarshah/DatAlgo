f8cb6c5de63c13d00ccd31383b2364e3784146dd
surendrajngp <surendraj454@gmail.com>
2021-05-15 14:41:10 +0530

    Home Page

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -5,7 +5,8 @@ import { algorithmsData } from "./algorithmsData";
 import { BsCodeSlash } from "react-icons/bs";
 import "./home.css";
 import Footer from "./footer/Footer";
-import homeImg from "./images/homeImg.png";
+import HomeTopComp from "./HomeTop";
+// import homeImg from "./images/homeImg.png";
 
 const Homepage = () => {
   return (
@@ -16,30 +17,8 @@ const Homepage = () => {
           paddingTop: "70px",
         }}
       >
-        <div className="homeTop container text-center pb-5 mx-auto">
-          <div className="row">
-            <div className="col-sm-6 homeHeadings d-flex align-items-center">
-              <div className="text-left">
-                <BsCodeSlash size={45} className="mb-2" />
-                <h1 className="text-left">
-                  <b style={{ color: "#0A4158" }}>Dat</b>
-                  <b style={{ color: "#4B8378" }}>Algo</b>
-                </h1>
-                <h3 className="pt-2">
-                  A tool to Visualize Data Structures & Algorithm
-                </h3>
-                <p className="text-danger mb-0 mt-2"># Learn Anytime, Anywhere</p>
-                <p className="text-danger m-0">
-                  # We believe everyone has the capacity to be creative.
-                </p>
-              </div>{" "}
-            </div>
-            <div className="col-sm-6 homeTopImg text-center">
-              <img src={homeImg} alt="Home" />
-            </div>
-          </div>
-        </div>
-
+        <HomeTopComp />
+        
         <div className="container-fluid">
           <div
             className="d-flex flex-wrap justify-content-center"
